94621565248e4bf894d95d7c1e5166ab412e00d3
Angel Velasquez <pcisavel@upc.edu.pe>
2022-04-26 10:29:50 -0500

    chore(ui): Declared PrimeVue Used Components.

--- src/main.js
+++ src/main.js
@@ -7,11 +7,23 @@ import PrimeVue from "primevue/config";
 import "primevue/resources/themes/md-light-indigo/theme.css";
 import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css";
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+import Toolbar from "primevue/toolbar";
+import InputText from "primevue/inputtext";
 
 const app = createApp(App);
 
 app.use(router);
 
 app.use(PrimeVue);
+
+// PrimeVue used components
+app.component("DataTable", DataTable);
+// eslint-disable-next-line vue/multi-word-component-names
+app.component("Column", Column);
+// eslint-disable-next-line vue/multi-word-component-names
+app.component("Toolbar", Toolbar);
+app.component("InputText", InputText);
 
 app.mount("#app");
